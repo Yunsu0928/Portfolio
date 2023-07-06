ecab90bb899ad11b157b3f1a7fcf46487368b24b
hongyunsu <dbstn9@hanmail.net>
2023-07-06 23:32:49 +0900

    [:construction:UI]: Main UI 작업 진행중

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+	/* width: 100%; */
+`;
+
+const FirstInner = styled.div`
+	background-color: navy;
+`;
+
 function App() {
 	return (
-		<div className="App">
-			<div></div>
-		</div>
+		<Container>
+			<FirstInner>안녕</FirstInner>
+		</Container>
 	);
 }
 
